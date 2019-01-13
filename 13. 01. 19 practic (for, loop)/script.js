cf8ade437a62371a4943178db54be5f37a540e4c
Viache <slavatyson@gmail.com>
2019-01-13 19:14:49 +0200

    array/for

---
+++ 13. 01. 19 practic (for, loop)/script.js
@@ -0,0 +1,29 @@
+var myStringArray = ['Hello', 'World'];
+var arrayLength = myStringArray.length;
+for(var i = 0; i < arrayLength; i++){
+	document.write('hello ')
+}
+
+document.write('<br>')
+
+var myArray1 = ['slava', 'vlad'];
+var arrayLength = myArray1.length; // width var arrayLength
+for(var y = 0; y < arrayLength; y++){
+	document.write('Hi ')
+}
+
+document.write('<br>');
+
+var arrayNew = ['Hello', 'World', 'Stas'];
+// widthout var arrayLength
+for(var t = 0; t < arrayNew.length; t++){
+	document.write('t ')
+}
+
+document.write('<br>');
+
+var sityArray = ['Dnipro', 'NY', 'LA', 'California'];
+var sityArrayLength = sityArray.length;
+for(var e = 0; e < sityArray.length; e++){
+	document.write(sityArray[e] + ' ');
+}
