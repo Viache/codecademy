f857ceb1d682a870a5e54bf47e57f8475cf2092f
Viache <slavatyson@gmail.com>
2019-01-13 18:06:31 +0200

    practic (for)

---
+++ 13.01.19 practic (for)/index.js
@@ -0,0 +1,140 @@
+for(var i = 0; i < 50; i++){
+	document.write('Number ' + i + ' ');
+}
+document.write('<br>');
+for (e = 0; e < 3; e++){
+	document.write(e)
+}
+document.write('<br>');
+for(r = 0; r < 3; r++){
+	document.write(r)
+}
+document.write('<br>');
+for(q = 0; q < 4; q++){
+	document.write('helo ' + q + '<br>')
+}
+document.write('<br>');
+for(g = 0; g < 5; g++){
+	document.write('five ')
+}
+document.write('<br>');
+for(v = 0; v < 4; v++){
+	document.write(v + ' hello ');
+}
+document.write('<br>');
+for(l = 0; l < 5; l++){
+	document.write('l ')
+}
+document.write('<br>');
+for(m = 0; m < 5; m++){
+	document.write('Hello')
+}
+document.write('<br>');
+for(h = 1; h <= 5; h++){
+	document.write(h);
+}
+document.write('<br>');
+for(cc = 2; cc < 12; cc++){
+	document.write(cc);
+}
+document.write('<br>');
+for(aa = 0; aa < 10; aa++){
+	document.write(aa + '<br>')
+}
+document.write('<br>');
+for(var tt = 0; tt < 10; tt++){
+	document.write(tt);
+}
+document.write('<br>');
+for(yy = 0; yy < 10; yy++){
+	if(yy % 3 == 0) continue;
+	document.write(yy);
+}
+document.write('<br>');
+for(nn = 0; nn < 10; nn++){
+	if(nn % 2 == 0) continue;
+	document.write(nn);
+}
+document.write('<br>');
+for(hh = 0; hh < 10; hh++){
+	if(hh % 4 == 0) continue;
+	document.write(hh);
+}
+document.write('<br>');
+document.write('<br>');
+for(jj = 0; jj < 10; jj++){
+	if(jj % 9 == 0) continue;
+	document.write(jj);
+}
+document.write('<br>');
+for(gg = 0; gg < 10; gg++){
+	if(gg == 0 || gg == 8 || gg == 9) continue;
+	document.write(gg);
+}
+document.write('<br>');
+for(pp = 0; pp < 10; pp++){
+	if(pp == 0 || pp == 7 || pp == 8 || pp == 9) continue;
+	document.write(pp)
+}
+document.write('<br>');
+for(rrr = 0; rrr < 6; rrr++){
+	if(rrr == 0) continue;
+	document.write(rrr)
+}
+document.write('<br>');
+for(fff = 0; fff < 5; fff++){
+	if(fff == 0) continue;
+	document.write(fff);
+}
+document.write('<br>');
+for(po = 0; po < 10; po++){
+	if(po == 0 || po == 4 || po == 5 || po == 6 || po == 7 || po == 8 || po == 9) continue;
+	document.write(po)
+}
+document.write('<br>')
+for(io = 1; io < 4; io++){
+	if(io == 3) continue;
+	document.write(io)
+}
+document.write('<br>');
+for(rt = 0; rt < 10; rt++){
+	if(rt == 0 || rt == 2 || rt == 3 || rt == 4 || rt == 5 || rt == 6 || rt == 7 || rt == 8 || rt == 9) continue;
+	document.write(rt);
+}
+document.write('<br>');
+for(eee = 0; eee < 10; eee++){
+	if(eee == 0 || eee == 3 || eee == 4 || eee == 5 || eee == 6 || eee == 7 || eee == 8 || eee == 9) continue;
+	document.write(eee);
+}
+document.write('<br>');
+for(yy = 1; yy < 4; yy++){
+	document.write(yy)
+}
+document.write('<br>')
+for(we = 1; we < 5; we++) {
+	document.write(we)
+}
+document.write('<br>')
+for(fg = 1; fg < 6; fg++){
+	document.write(fg)
+}
+document.write('<br>')
+for(vv = 0; vv < 10; vv++){
+	if(vv == 0 || vv == 7 || vv == 8 || vv == 9) continue;
+	document.write(vv)
+}
+document.write('<br>')
+for(uu = 0; uu < 10; uu++){
+	if(uu == 0 || uu == 8 || uu == 9) continue;
+	document.write(uu)
+}
+document.write('<br>')
+for(lll = 0; lll < 10; lll++){
+	if(lll == 0 || lll == 9) continue;
+	document.write(lll)
+}
+document.write("<br>")
+for(dfdf = 0; dfdf < 100; dfdf++){
+	if(dfdf % 34.5 == 0) continue;
+	document.write(dfdf + ' ')
+}
