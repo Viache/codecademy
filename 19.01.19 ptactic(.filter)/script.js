815e121e66664288e60dba27ec46396eb2d1ddc3
Viache <slavatyson@gmail.com>
2019-01-19 20:39:48 +0200

    practic

---
+++ 19.01.19 ptactic(.filter)/script.js
@@ -0,0 +1,24 @@
+
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+const smallNumbers = randomNumbers.filter(ranNum => {
+  return ranNum > 250;
+});
+document.write(smallNumbers);
+
+document.write('<br>');
+
+const randomNumbers2 = [234, 34, 4, 35, 45622, 1235, 5, 22];
+const smallNumbers2 = randomNumbers2.filter(num2 => {
+	return num2 > 100;
+});
+document.write(smallNumbers2);
+
+document.write('<br>');
+
+const someWords = ['asdasd', 'sd', 'safawe', 'd', 'dasdf', 'd'];
+const longWords = someWords.filter(long => {
+	return long.length > 3;
+});
+document.write(longWords);
