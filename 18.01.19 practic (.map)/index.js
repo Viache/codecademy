ba5235b9eec697d1d9f3defe96437b2363b115a9
Viache <slavatyson@gmail.com>
2019-01-19 20:02:07 +0200

    add number.map

--- 18.01.19 practic (.map)/index.js
+++ 18.01.19 practic (.map)/index.js
@@ -45,3 +45,64 @@ const WoW = okay.map(WoWmap =>
 	WoWmap[0]);
 document.write('I love ' + WoW.join('') + '.');
 
+
+
+
+document.write('<br>');
+
+const bigNumbers = [100, 200, 300, 400, 500];
+const smallNumbers = bigNumbers.map(num =>
+	num/100);
+
+document.write(smallNumbers);
+
+document.write('<br>');
+
+bigNumbers2 = [456, 345, 576, 2345, 546];
+smallNumbers2 = bigNumbers2.map(num2 =>
+	num2/100);
+document.write(smallNumbers2);
+
+document.write('<br>');
+
+const bigNumbers3 = [34345, 234, 36, 67, 245];
+const smallNumbers3 = bigNumbers3.map(num3 =>
+	num3 * 3);
+document.write(smallNumbers3);
+
+document.write('<br>');
+
+const bigNumbers4 = [456, 234, 234, 456, 567];
+const smallNumbers4 = bigNumbers4.map(num4 =>
+	num4 + 1000);
+document.write(smallNumbers4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
